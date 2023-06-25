0062317978b0a114aa8dc5935d48007e266ac635
Jack Stubblefield <jStubblefield@lbisit.onmicrosoft.com>
2023-06-25 13:19:53 -0500

    added plaid token variables

--- server.ts
+++ server.ts
@@ -1,33 +1,91 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
-import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+import { Configuration, PlaidApi, PlaidEnvironments, Products } from 'plaid';
+require('dotenv').config();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+const util = require('util');
 
-import dotenv from 'dotenv';
+const CLIENT_ID = process.env.PLAID_CLIENT_ID
+const SECRET = process.env.PLAID_SECRET
+const PORT = process.env.PORT;
 
-dotenv.config();
+// PLAID_PRODUCTS is a comma-separated list of products to use when initializing
+// Link. Note that this list must contain 'assets' in order for the app to be
+// able to create and retrieve asset reports.
+const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || Products.Transactions).split(
+  ',',
+);
+
+// PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
+// will be able to select institutions from.
+const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(
+  ',',
+);
+
+// Parameters used for the OAuth redirect Link flow.
+//
+// Set PLAID_REDIRECT_URI to 'http://localhost:3000'
+// The OAuth redirect flow requires an endpoint on the developer's website
+// that the bank website should redirect to. You will need to configure
+// this redirect URI for your client ID through the Plaid developer dashboard
+// at https://dashboard.plaid.com/team/api.
+const PLAID_REDIRECT_URI = process.env.PLAID_REDIRECT_URI || '';
+
+// Parameter used for OAuth in Android. This should be the package name of your app,
+// e.g. com.plaid.linksample
+const PLAID_ANDROID_PACKAGE_NAME = process.env.PLAID_ANDROID_PACKAGE_NAME || '';
+
+// We store the access_token in memory - in production, store it in a secure
+// persistent data store
+let ACCESS_TOKEN: string | null = null;
+let PUBLIC_TOKEN: string | null = null;
+let ITEM_ID: string | null = null;
+// The payment_id is only relevant for the UK/EU Payment Initiation product.
+// We store the payment_id in memory - in production, store it in a secure
+// persistent data store along with the Payment metadata, such as userId .
+let PAYMENT_ID = null;
+// The transfer_id is only relevant for Transfer ACH product.
+// We store the transfer_id in memory - in production, store it in a secure
+// persistent data store
+let TRANSFER_ID = null;
+
+// Initialize the Plaid client
+// Find your API keys in the Dashboard (https://dashboard.plaid.com/account/keys)
 
 const configuration = new Configuration({
   basePath: PlaidEnvironments.development,
   baseOptions: {
     headers: {
-      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
-      'PLAID-SECRET': process.env.PLAID_SECRET,
+      'PLAID-CLIENT-ID': CLIENT_ID,
+      'PLAID-SECRET': SECRET,
     },
   },
 });
 
+
+
 const app: Express = express();
-const port = process.env.PORT;
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+app.use(bodyParser.json());
+app.use(cors());
+
 const client = new PlaidApi(configuration);
 
 app.get('/', (req: Request, res: Response): void => {
   res.send('Express + TypeScript Server');
 });
 
-app.use('/get', (req: Request, res: Response): void => {
-  const response = await plaidClient.itemPublicTokenExchange({ public_token });
-  const access_token = response.data.access_token;
-  const accounts_response = await plaidClient.accountsGet({ access_token });
-  const accounts = accounts_response.data.accounts;
+app.post('/api/info', function (request, response, next) {
+  response.json({
+    item_id: ITEM_ID,
+    access_token: ACCESS_TOKEN,
+    products: PLAID_PRODUCTS,
+  });
 });
 
 
@@ -56,6 +114,6 @@ app.post('/api/create_link_token', async function (res: Response, req: Request )
 });
 
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
