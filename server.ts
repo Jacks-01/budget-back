ed8b161efd9d8eb25519b251f9a6c4e98fcf5d39
Jack Stubblefield <jStubblefield@lbisit.onmicrosoft.com>
2023-06-24 21:50:07 -0500

    tried implementing plaid types

--- server.ts
+++ server.ts
@@ -1,17 +1,33 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.development,
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+      'PLAID-SECRET': process.env.PLAID_SECRET,
+    },
+  },
+});
+
 const app: Express = express();
 const port = process.env.PORT;
+const client = new PlaidApi(configuration);
 
 app.get('/', (req: Request, res: Response): void => {
   res.send('Express + TypeScript Server');
 });
 
 app.use('/get', (req: Request, res: Response): void => {
-
+  const response = await plaidClient.itemPublicTokenExchange({ public_token });
+  const access_token = response.data.access_token;
+  const accounts_response = await plaidClient.accountsGet({ access_token });
+  const accounts = accounts_response.data.accounts;
 });
 
 
