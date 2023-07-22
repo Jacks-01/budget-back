aff45e6db92eaafb7532a94167356090163cceb4
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2023-07-22 18:06:52 -0500

    added prisma service to api

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import {
 	LinkTokenCreateRequest,
 	PlaidApi,
 	PlaidEnvironments,
-  Products,
-  CountryCode
+	Products,
+	CountryCode
 } from 'plaid';
 require('dotenv').config();
 const cors = require('cors');
@@ -14,15 +14,15 @@ const { v4: uuidv4 } = require('uuid');
 const util = require('util');
 const axios = require('axios');
 
-const CLIENT_ID: string = process.env.PLAID_CLIENT_ID || ''
+const CLIENT_ID: string = process.env.PLAID_CLIENT_ID || '';
 const SECRET = process.env.PLAID_SECRET;
 const PORT = process.env.PORT;
 const BASE_URL = process.env.BASE_URL;
 
+import { Prisma, PrismaClient } from '@prisma/client';
 
-// PLAID_PRODUCTS is a comma-separated list of products to use when initializing
-// Link. Note that this list must contain 'assets' in order for the app to be
-// able to create and retrieve asset reports.
+const prisma = new PrismaClient();
+
 const PLAID_PRODUCTS = (
 	process.env.PLAID_PRODUCTS || Products.Transactions
 ).split(',');
@@ -33,13 +33,6 @@ const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(
 	','
 );
 
-// Parameters used for the OAuth redirect Link flow.
-//
-// Set PLAID_REDIRECT_URI to 'http://localhost:3000'
-// The OAuth redirect flow requires an endpoint on the developer's website
-// that the bank website should redirect to. You will need to configure
-// this redirect URI for your client ID through the Plaid developer dashboard
-// at https://dashboard.plaid.com/team/api.
 const PLAID_REDIRECT_URI = process.env.PLAID_REDIRECT_URI || '';
 
 // We store the access_token in memory - in production, store it in a secure
@@ -85,42 +78,40 @@ app.post(
 );
 
 app.get('/create_link_token', async function (req: Request, res: Response) {
-  // Get the client_user_id by searching for the current user
+	// Get the client_user_id by searching for the current user
 	console.log(`GET REQUEST:`);
-  const products: Products[] = [Products.Auth]
-  const country_codes: CountryCode[] =  [CountryCode.Us]
-  const request: LinkTokenCreateRequest = {
-    user: {
-      // This should correspond to a unique id for the current user.
-      client_user_id: CLIENT_ID,
-    },
-    client_name: 'Jack Stubblefield',
-    products: products,
-    language: 'en',
-    country_codes: country_codes,
-  };
-  try {
-    const createTokenResponse = await client.linkTokenCreate(request);
-    res.json(createTokenResponse.data);
-    console.log(createTokenResponse)
-  } catch (error) {
-    // handle error
-    res.send(error)
-  }
+	const products: Products[] = [Products.Auth];
+	const country_codes: CountryCode[] = [CountryCode.Us];
+	const request: LinkTokenCreateRequest = {
+		user: {
+			// This should correspond to a unique id for the current user.
+			client_user_id: CLIENT_ID
+		},
+		client_name: 'Jack Stubblefield',
+		products: products,
+		language: 'en',
+		country_codes: country_codes
+	};
+	try {
+		const createTokenResponse = await client.linkTokenCreate(request);
+		res.json(createTokenResponse.data);
+		console.log(createTokenResponse);
+	} catch (error) {
+		// handle error
+		res.send(error);
+	}
 });
-
 
 app.post('/tokenInfo', async (req: Request, res: Response) => {
 	console.log(req.body);
-	let ACCESS_TOKEN = req.body.ACCESS_TOKEN
-	let PUBLIC_TOKEN = req.body.PUBLIC_TOKEN
-
-})
+	let ACCESS_TOKEN = req.body.ACCESS_TOKEN;
+	let PUBLIC_TOKEN = req.body.PUBLIC_TOKEN;
+});
 
 // const prettyPrintResponse = (res) => {
 //   console.log(util.inspect(res.data, { colors: true, depth: 4 }));
 // };
 
 app.listen(PORT, () => {
-	console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+	console.log(`🚀[server]: Server is running at http://localhost:${PORT}`);
 });
